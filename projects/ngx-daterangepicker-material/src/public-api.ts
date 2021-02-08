1bb6a6a169c7b0cd8b741145e177374af063fd13
anderstornkvist <dev@anderstornkvist.se>
2021-02-08 12:30:30 +0100

    chore: convert to angular 11 library format

---
+++ projects/ngx-daterangepicker-material/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ngx-daterangepicker-material
+ */
+
+export { NgxDaterangepickerMd } from './lib/daterangepicker.module';
+export { DaterangepickerComponent } from './lib/daterangepicker.component';
+export { DaterangepickerDirective } from './lib/daterangepicker.directive';
+export { LocaleConfig, LOCALE_CONFIG, DefaultLocaleConfig } from './lib/daterangepicker.config';
+export { LocaleService } from './lib/locale.service';
+
